68292e54e4ddef16cbe9c0592f06ec3e6a20ae8a
Srinath Sankar <srinath@iambot.net>
2020-02-25 18:26:28 +1100

    Tests for #4
    Fixes #3

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
-_cleanOutput() {
+_trimColors() {
     while read -r line; do
         # shellcheck disable=2001
-        sed "s,$(printf '\033')\\[[0-9;]*[a-zA-Z],,g" <<< "$line" | awk '{$1=$1;print}' | awk /./
+        sed "s,$(printf '\033')\\[[0-9;]*[a-zA-Z],,g" <<< "$line"
+    done
+}
+
+_trimOutput() {
+    while read -r line; do
+        echo "$line" | awk '{$1=$1;print}' | awk /./
     done
 }
 
@@ -14,11 +20,7 @@ _abspath() {
 
 _runTest() {
     local file="$1"
-    local expectedCode="$2"
-    local expectedOutput="$3"
-    local output code
-
-    echo "--- Running $file"
+    local expectedCode="${2:-}"
 
     output="$(bash "$file")"
     code=$?
@@ -29,19 +31,14 @@ _runTest() {
         echo "Exit codes do not match. Actual: $code, expected: $expectedCode"
         exitCode=$code
     fi
-
-    if ! diff -bBEi \
-        <(sed -n -e '/\[critic\] Coverage Report/,$p' <<< "$output" | _cleanOutput | tail -n +2 | head -n -1) \
-        <(echo "$expectedOutput"); then
-        exitCode=1
-    else
-        echo "Outputs match"
-    fi
 }
 
 exitCode=0
 
-expected="$(cat <<EOF
+echo "--- Coverage report"
+_runTest "examples/test.sh" 1
+
+expectedCoverage="$(cat <<EOF
 $(pwd)/examples/lib.sh
 Total LOC: 19
 Covered LOC: 3
@@ -50,7 +47,28 @@ Ignored LOC: 5
 Uncovered Lines: 21 22 30
 EOF
 )"
+if ! diff -bBEi \
+    <(sed -n -e '/\[critic\] Coverage Report/,$p' <<< "$output" | _trimColors | _trimOutput | tail -n +2 | head -n -1) \
+    <(echo "$expectedCoverage"); then
+    exitCode=1
+else
+    echo "Coverage report matches"
+fi
 
-_runTest "examples/test.sh" 1 "$expected"
+echo "--- _output_contains"
+expectedOutput="$(cat <<EOF
+readme
+Should print readme
+PASS ✔ : Readme contains options
+PASS ✔ : Output contains 'critic.sh'
+EOF
+)"
+if ! diff -bBEi \
+    <(echo "$output" | _trimColors | awk '/^readme/,/^ *$/' | _trimOutput) \
+    <(echo "$expectedOutput"); then
+    exitCode=1
+else
+    echo "Output matches"
+fi
 
 exit $exitCode
